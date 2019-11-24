026c2622649b549a6776cc7405037518e09e9c9c
Natasha S Love <debora@inbox.ru>
2019-11-24 03:31:13 -0600

    showing icon
    from libraries

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+//the lib is already in nodes- just picking exact style/library - Feather 
+//on expo.github.io/vector-icons
 
 const SearchBar = () => {
 
     return <View style={styles.background}>
+        <Feather name="search" size={30} />
         <Text>Search Bar</Text>
 
 
@@ -15,7 +19,7 @@ const styles = StyleSheet.create({
     background: {
         backgroundColor: '#f0eeee',
         height:50,
-        borderRadius: 5,
+        borderRadius: 5,// rounded corners
         marginHorizontal: 15
     }
 
