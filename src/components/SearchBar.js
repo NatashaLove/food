f529e27917b387011543b1ac7d6fc1cb38780350
Natasha S Love <debora@inbox.ru>
2019-11-26 20:19:58 -0600

    Search bar component
    style, properties and callback functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, TextInput, StyleSheet} from 'react-native';//textInput - for user to input
 import { Feather } from '@expo/vector-icons';
 //the lib is already in nodes- just picking exact style/library - Feather 
 //on expo.github.io/vector-icons
 
-const SearchBar = () => {
+//destructorizing properties of the SearchBar({}):
+//props help to manage the search bar object from the parent
+const SearchBar = ({term, onTermChange, onTermSubmit }) => {
 
-    return <View style={styles.background}>
-        <Feather name="search" size={30} />
-        <Text>Search Bar</Text>
-
+    return <View style={styles.backgroundStyle}>
+        <Feather name="search" style= {styles.iconStyle} />
+        <TextInput 
+        autoCapitalize="none"
+        autoCorrect={false} // in case it's proper name - not to autocorrect
+        style ={styles.inputStyle} 
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+        //the same almost:
+        //onChangeText={newTerm => onTermChange(newTerm)}
+        //onEndEditing={()=> onTermSubmit()} // when submit button is clicked
+        />
 
     </View>
 };
-
+// placeholder - property of textInput (prompt)
+//all the style of the search bar:
 const styles = StyleSheet.create({
 
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#f0eeee',
         height:50,
         borderRadius: 5,// rounded corners
+        marginHorizontal: 15,
+        flexDirection: 'row' // makes the icon and the text show on the same line   
+    },
+    inputStyle: {
+        //borderColor: 'black',
+        //borderWidth: 1,
+        flex: 1, // to use as much space as can
+        fontSize: 18
+    },
+
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
         marginHorizontal: 15
     }
 
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
 
-    return <View>
-        <SearchBar />
+    //term - what we are going to look for:
+    const [term, setTerm] = useState(''); //empty string by default
+
+
+    //callback functions to the child - when there are changes in the state:
+   //-to reach search bar from the parent component
+    return (
+     <View>
+        <SearchBar term={term} 
+        onTermChange = {newTerm => setTerm(newTerm)} 
+        onTermSubmit={() => console.log('term was submitted')}
+        />
         <Text>Search Screen</Text>
+        <Text>{term}</Text>
 
 
     </View>
+    );
 };
 
 const styles = StyleSheet.create({});
