024e31542fbc72474bf06fcdf707e3710dae1e70
Natasha S Love <debora@inbox.ru>
2019-12-10 21:00:16 -0600

    Create ResultsList.js
    new component - which will be used on 1 screen 3 times- with resultsList

---
+++ src/components/ResultsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+//creating the component with view and style
+const ResultsList = ()=> {
+
+    return <View>
+        <Text>ResultsList</Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsList;
