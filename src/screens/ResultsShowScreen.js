6f731501bba20ff96f876d9f349f3efcd7a05183
Natasha S Love <debora@inbox.ru>
2020-01-21 19:21:19 -0600

    getting results of 1 obj
    getting the name and images ( can be added any other data)

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -2,21 +2,22 @@
 //so that a user can navigate to it anytime they tap on one of these different restaurants.
 import React, { useState, useEffect } from 'react';
 //So we need to create a new State variable inside of this component that's going to hold onto this response which we get back from the Yelp API
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native';// Image class - to show images
+//FlatList - to display the result: list of elements (pics)
 import ResultsDetail from '../components/ResultsDetail';
 import yelp from '../api/yelp';
 //need to use that Yelp instance (or the Axios instance) inside of our API: Yelp.js file
 
 const ResultsShowScreen = ({navigation}) => {
 // needs to accept 'navigation' prop in order to get that information inside of results show screen
-
+//'result' originally = null
 const [result, setResult] = useState(null);// result - new piece of state/ null - no data available (before request)
 const id = navigation.getParam('id');//to get some information out of that second argument that we called navigate with (id)
 //assign that info about real id to a variable 'id'
 //use ID to make a request to the Yelp API.
 
 //console.log(id); - shows id # of the business
-console.log(result); // details of particular restaurant
+//console.log(result); // details of particular restaurant
 
 //we're going to call getResults and we're going to pass in the I.D. that we want to retrieve:
 const getResult = async (id) => {
@@ -38,14 +39,35 @@ useEffect (() => {
     getResult(id);
 }, []);
 
+//error check:
+if (!result){
+    return null;
+// if there is no result yet ('result' is null)- return null= do not show anything on the screen 
+//until we get some kind of result.
+// we can add some error handling to show an error message instead of just 'null'.    
+}
+
 return (
 <View>
-    <Text>Results Show</Text>
-</View>
+    <Text>{result.name}</Text> 
+    <FlatList
+    data={result.photos}// arr of strings
+    keyExtractor={(photo) => photo}// photo - is a string (url of the img)-unique string - that's why it's key and return
+    renderItem= {({item}) => {
+// 'item' property is going to be the actual photo string (U.R.L. of the photo we're trying to show)
+        return <Image style={styles.image} source={{ uri:item}} />
+    }}// {{}}- outer to show it's javascript, inner - the actual obj
+    />
+    </View>
 );
 
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300
+    }
+});
 
 export default ResultsShowScreen;
